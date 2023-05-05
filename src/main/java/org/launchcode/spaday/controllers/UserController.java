7b36abfe57857686bb0d7911c4f28a378c2225c7
Noobwastaken99 <125329223+Noobwastaken99@users.noreply.github.com>
2023-05-04 20:31:33 -0500

    90% studio finished

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -14,30 +14,23 @@ import java.util.*;
 @RequestMapping("user")
 public class UserController {
 
-    private static List<User> users = new ArrayList<>();
-
     @GetMapping
-    public String customerForm (Model model) {
-        model.addAttribute("username", "allU");
-        model.addAttribute("email", "allE");
-        model.addAttribute("password", "allP");
+    public String user () {
         return "user/index";
     }
 
     @GetMapping("add")
-    public String displayAddUserForm(Model model) {
-//        model.addAttribute();
-//        model.addAttribute();
-//        model.addAttribute();
+    public String displayAddUserForm() {
         return "user/add";
     }
 
     @PostMapping("add")
     public String displayAddUserForm(Model model, @ModelAttribute User user, String verify) {
-//        model.addAttribute();
-//        model.addAttribute();
-//        model.addAttribute();
-        return "redirect:";
+        if(user.getPassword().equals(verify)) {
+            return "redirect:";
+        }
+        model.addAttribute("username", user.getUsername());
+        return "user/add";
     }
 
 }
